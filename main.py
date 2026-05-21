43bec7d28670d953817f1a926fca6fcbd1a4b257
rendor535 <marcinbos2004@gmail.com>
2026-05-21 18:55:44 +0200

    Confusion matrix maker

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from image_loader import ImageLoader
 from feature_extractor import FeatureExtractor
 from presenter import Presenter
 from model import Model
+from classification_reporter import ClassificationReporter
 
 loader = ImageLoader()
 images, masks, file_names, labels = loader.load_all()
 
 correct_sum = 0
 incorrect_sum = 0
+reporter = ClassificationReporter()
 for i in range(40):
     model = Model((images, masks, labels), test_percentage=0.1)
     correct_count, incorrect_count = model.Test()
@@ -15,6 +17,7 @@ for i in range(40):
     incorrect_sum += incorrect_count
     # print(correct_count, incorrect_count)
     # print(correct_count / (incorrect_count + correct_count) * 100)
-
+    reporter.add_iteration(model)
+reporter.run()
 print(correct_sum, incorrect_sum)
 print(correct_sum / (incorrect_sum + correct_sum) * 100)
